34525c5777124f87c3f4915fe79e12b1ad923f28
Egor Merkushev <gorkaedeep@gmail.com>
2025-10-08 23:58:18 +0300

    Fix duplicate markdown headings for rule sections

---
+++ Sources/ISOInspectorApp/State/ParseTreeSnapshot.swift
@@ -0,0 +1,22 @@
+#if canImport(Combine)
+import Foundation
+import ISOInspectorKit
+
+public struct ParseTreeSnapshot: Equatable {
+    public var nodes: [ParseTreeNode]
+    public var validationIssues: [ValidationIssue]
+    public var lastUpdatedAt: Date
+
+    public init(
+        nodes: [ParseTreeNode],
+        validationIssues: [ValidationIssue],
+        lastUpdatedAt: Date = .distantPast
+    ) {
+        self.nodes = nodes
+        self.validationIssues = validationIssues
+        self.lastUpdatedAt = lastUpdatedAt
+    }
+
+    public static let empty = ParseTreeSnapshot(nodes: [], validationIssues: [], lastUpdatedAt: .distantPast)
+}
+#endif
---
+++ Sources/ISOInspectorApp/State/ParseTreeStoreState.swift
@@ -0,0 +1,8 @@
+#if canImport(Combine)
+public enum ParseTreeStoreState: Equatable {
+    case idle
+    case parsing
+    case finished
+    case failed(String)
+}
+#endif
