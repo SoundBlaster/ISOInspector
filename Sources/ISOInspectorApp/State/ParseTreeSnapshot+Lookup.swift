be39d1b186ca857a5b26f8c306c6e311b4a11693
Egor Merkushev <gorkaedeep@gmail.com>
2025-10-23 10:29:09 +0300

    app: introduce document view models

---
+++ Sources/ISOInspectorApp/State/ParseTreeSnapshot+Lookup.swift
@@ -0,0 +1,21 @@
+#if canImport(Combine)
+import ISOInspectorKit
+
+extension ParseTreeSnapshot {
+    func containsNode(with id: ParseTreeNode.ID) -> Bool {
+        nodes.contains { $0.containsNode(with: id) }
+    }
+}
+
+private extension ParseTreeNode {
+    func containsNode(with id: ParseTreeNode.ID) -> Bool {
+        if self.id == id {
+            return true
+        }
+        for child in children where child.containsNode(with: id) {
+            return true
+        }
+        return false
+    }
+}
+#endif
