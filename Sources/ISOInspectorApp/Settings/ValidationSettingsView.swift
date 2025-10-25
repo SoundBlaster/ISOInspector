93781dd82b3a1aa231594662d41d4c203d38afae
Egor Merkushev <gorkaedeep@gmail.com>
2025-10-25 14:00:09 +0300

    Use onChangeCompat for backwards-compatible onChange handling

--- Sources/ISOInspectorApp/Settings/ValidationSettingsView.swift
+++ Sources/ISOInspectorApp/Settings/ValidationSettingsView.swift
@@ -25,7 +25,7 @@ struct ValidationSettingsView: View {
             }
         }
         .padding()
-        .onChange(of: controller.currentDocument) { document in
+        .onChangeCompat(of: controller.currentDocument) { document in
             if document == nil {
                 selectedScope = .global
             }
@@ -158,6 +158,24 @@ struct ValidationSettingsView: View {
                 controller.setValidationRule(rule, isEnabled: newValue, scope: effectiveScope)
             }
         )
+    }
+}
+
+extension View {
+    @ViewBuilder
+    fileprivate func onChangeCompat<Value: Equatable>(
+        of value: Value,
+        perform action: @escaping (Value) -> Void
+    ) -> some View {
+        if #available(iOS 17.0, macOS 14.0, *) {
+            self.onChange(of: value, initial: false) { _, newValue in
+                action(newValue)
+            }
+        } else {
+            self.onChange(of: value) { newValue in
+                action(newValue)
+            }
+        }
     }
 }
 #endif
