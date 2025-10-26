33ff7ae50a71349ef1feeccd3200bee8ece5422c
Egor Merkushev <gorkaedeep@gmail.com>
2025-10-26 14:08:09 +0300

    Refine Tuist release helper parsing

--- Sources/ISOInspectorApp/Settings/ValidationSettingsView.swift
+++ Sources/ISOInspectorApp/Settings/ValidationSettingsView.swift
@@ -161,21 +161,4 @@ struct ValidationSettingsView: View {
     }
 }
 
-extension View {
-    @ViewBuilder
-    fileprivate func onChangeCompat<Value: Equatable>(
-        of value: Value,
-        perform action: @escaping (Value) -> Void
-    ) -> some View {
-        if #available(iOS 17.0, macOS 14.0, *) {
-            self.onChange(of: value, initial: false) { _, newValue in
-                action(newValue)
-            }
-        } else {
-            self.onChange(of: value) { newValue in
-                action(newValue)
-            }
-        }
-    }
-}
 #endif
