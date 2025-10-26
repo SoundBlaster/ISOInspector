27a67e1bec84a3968c30becfa8af5f7d0db2a522
Egor Merkushev <gorkaedeep@gmail.com>
2025-10-27 00:01:23 +0300

    Rebundle FoundationUI in main manifest

--- Sources/FoundationUI/FoundationUI.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Namespace for utilities that identify the FoundationUI Swift package.
-public enum FoundationUI {
-    /// Stable identifier describing the FoundationUI module.
-    public static let moduleIdentifier: String = "FoundationUI"
-}
