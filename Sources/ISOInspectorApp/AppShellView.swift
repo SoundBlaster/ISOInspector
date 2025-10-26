3288406f68822868849cfab4b98fbb6082147096
Egor Merkushev <gorkaedeep@gmail.com>
2025-10-26 13:59:32 +0300

    Fix macOS CI workflow and SwiftUI helpers

--- Sources/ISOInspectorApp/AppShellView.swift
+++ Sources/ISOInspectorApp/AppShellView.swift
@@ -355,24 +355,6 @@ private struct CorruptionWarningRibbon_Previews: PreviewProvider {
         }
     }
 }
-
-extension View {
-    @ViewBuilder
-    fileprivate func onChangeCompat<Value: Equatable>(
-        of value: Value,
-        perform action: @escaping (Value) -> Void
-    ) -> some View {
-        if #available(iOS 17.0, macOS 14.0, *) {
-            self.onChange(of: value, initial: false) { _, newValue in
-                action(newValue)
-            }
-        } else {
-            self.onChange(of: value) { newValue in
-                action(newValue)
-            }
-        }
-    }
-}
 #endif
 
 private struct DocumentLoadFailureBanner: View {
