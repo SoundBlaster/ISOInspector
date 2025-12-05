5a9e615720e55798bc42532e400ee81f2f2bfc6f
Egor Merkushev <gorkaedeep@gmail.com>
2025-12-06 00:55:15 +0300

    Change isAccessibilityCategory from fileprivate to private

--- FoundationUI/Sources/FoundationUI/Contexts/AccessibilityContext.swift
+++ FoundationUI/Sources/FoundationUI/Contexts/AccessibilityContext.swift
@@ -115,7 +115,7 @@ public struct AccessibilityContext: Equatable, Sendable {
 
 extension DynamicTypeSize {
     /// Indicates whether the dynamic type size is an accessibility category.
-    fileprivate var isAccessibilityCategory: Bool { self >= .accessibility1 }
+    private var isAccessibilityCategory: Bool { self >= .accessibility1 }
 }
 
 /// Overrides that can be applied to the derived accessibility context.
